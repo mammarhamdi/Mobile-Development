7b6f55c90fc20f43fd7f8038ad922a9acc5571fb
Muhammad Rizki Dalfi <muhammad.rizki3405@student.unri.ac.id>
2024-05-29 23:27:48 +0700

    add feature : add UI for Welcome Activity

--- src/main/java/com/bangkit/eyetify/MainActivity.kt
+++ src/main/java/com/bangkit/eyetify/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.bangkit.eyetify.databinding.ActivityMainBinding
 import com.bangkit.eyetify.ui.activity.WelcomeActivity
